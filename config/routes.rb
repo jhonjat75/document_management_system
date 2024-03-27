4144e7d1e450095a2142fd91a42d6dabfceabae5
Jhon mac <jhonjairotorres75@gmail.com>
2024-03-27 07:52:57 -0500

    feat: user crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'users/index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  root to: "users#index"
+  resources :users, only: [:index]
 end
